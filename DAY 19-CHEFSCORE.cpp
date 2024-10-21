55448f8a0b45fecf46f84ac58d3e24936445000d
anurag477 <anuraga1507x@gmail.com>
2024-10-21 21:35:42 +0530

    Add files via upload

---
+++ DAY 19-CHEFSCORE.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; 
+    
+    while (T--) {
+        int N, X, Y;
+        cin >> N >> X >> Y; 
+        
+       
+        int maxScore = N * X;
+        
+       
+        if (Y <= maxScore && Y % X == 0) {
+            cout << "YES" << endl; 
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    
+    return 0;
+}
