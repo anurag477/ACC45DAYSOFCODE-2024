1d8f98a56d60932a10f9a93dbcccaee733be602b
anurag477 <anuraga1507x@gmail.com>
2024-11-11 21:55:27 +0530

    Add files via upload

---
+++ DAY 40-MOVIE2X.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int X, Y;
+    cin >> X >> Y;
+    
+  
+    int totalTime = (Y / 2) + (X - Y);
+    
+   
+    cout << totalTime << endl;
+    
+    return 0;
+}
