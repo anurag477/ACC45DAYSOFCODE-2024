4e492d7584b152d381477e9ded60ab7c050abd56
anurag477 <anuraga1507x@gmail.com>
2024-11-12 18:04:05 +0530

    Add files via upload

---
+++ DAY 41-BATH.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T; 
+    cin >> T;  
+    
+   
+    while (T--) {
+        int X, Y; 
+        cin >> X >> Y; 
+        
+       
+        int water_required_per_person = 2 * Y; 
+        int max_people = X / water_required_per_person; 
+        
+        cout << max_people << endl; 
+    }
+    
+    return 0;
+}
